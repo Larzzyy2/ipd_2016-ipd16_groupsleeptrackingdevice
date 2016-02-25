588292a6119820aa9886ba7461b5e4d6c429310f
Sven Bollaerts BOLLE <sven.bollaerts@hotmail.com>
2016-02-25 10:04:40 +0100

    Code v1

---
+++ app/app/src/main/java/com/example/bollez/sleeptrackingdevice/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.bollez.sleeptrackingdevice;
+
+import android.os.Bundle;
+
+/**
+ * Created by Bollez on 25/02/2016.
+ */
+public class SignUp {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.LoginActivity);
+    }
+}
