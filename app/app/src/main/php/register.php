682cd003c7585ea93a626a88ed17ff66035aa37a
Lars <larssuffys@hotmail.com>
2016-03-03 11:48:05 +0100

    updated register.php

--- app/app/src/main/php/register.php
+++ app/app/src/main/php/register.php
@@ -8,16 +8,16 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 		$gender = $_POST['gender'];
 		$timezone = $_POST['timezone];
 		
-		if($lastname == '' || $name == '' || $password == '' || $email == ''){
+		if($lastname == '' || $name == '' || $password == '' || $email == '' || $yob =='' || $gender =='' || $timezone == ''){
 			echo 'please fill all values';
 		}else{
 			require_once('dbconnect.php');
-			$sql = "SELECT * FROM User WHERE username='$username' OR email='$email'";
+			$sql = "SELECT * FROM User WHERE email='$email'";
 			
 			$check = mysqli_fetch_array(mysqli_query($con,$sql));
 			
 			if(isset($check)){
-				echo 'username or email already exist';
+				echo 'email already exist';
 			}else{				
 				$sql = "INSERT INTO users (Last_Name,Name,E-Mail_Address,Password,YoB,Gender,Time_Zone) VALUES('$lastname','$name','$password','$email','$yob','$gender','$timezone')";
 				if(mysqli_query($con,$sql)){
