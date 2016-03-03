124d0815796488e569acf26e5b97c674ac3c46f7
Lars <larssuffys@hotmail.com>
2016-03-03 09:54:35 +0100

    Made better login activity

--- app/app/src/main/java/com/example/bollez/sleeptrackingdevice/LoginActivity.java
+++ app/app/src/main/java/com/example/bollez/sleeptrackingdevice/LoginActivity.java
@@ -5,25 +5,48 @@ package com.example.bollez.sleeptrackingdevice;
         import android.content.Intent;
         import android.os.Bundle;
         import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
         import android.widget.TextView;
 
 public class LoginActivity extends Activity {
+    EditText username;
+    EditText password;
+    Button login;
+    int Attempts;
+
     @Override
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setting default screen to login.xml
         setContentView(R.layout.login);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.loginbutton);
+        Attempts = 3;
 
-        TextView registerScreen = (TextView) findViewById(R.id.link_to_register);
 
-        // Listening to register new account link
-        registerScreen.setOnClickListener(new View.OnClickListener() {
 
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                // Switching to Register screen
-                Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
-                startActivity(i);
+                if (Attempts != 0) {
+                    if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+
+                        //correct
+                    } else {
+                        //wrong password
+                        Attempts--;
+                    }
+                }
+                else
+                {
+                    login.setEnabled(false);
+                }
             }
         });
     }
+
+
 }
